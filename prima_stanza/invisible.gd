e361785cccbac915d9be8fd0b1d24e1ec6057b4b
Emmanuele_Cogoni <cogoni279@gmail.com>
2025-07-24 23:28:01 +0200

    implementato mesh tile e usato stickman
    telecamera da discutere

--- prima_stanza/invisible.gd
+++ prima_stanza/invisible.gd
@@ -1,7 +1,7 @@
 extends Area3D
 
 @onready var invisible=$".."
-#@onready var camera=$"../../../CharacterBody3D/Camera_principale"
+@onready var camera=$"../../../CharacterBody3D/Camera_principale"
 
 func _on_body_entered(body):
 	invisible.material.transparency=BaseMaterial3D.TRANSPARENCY_ALPHA
@@ -10,5 +10,5 @@ func _on_body_entered(body):
 
 func _on_body_exited(body):
 	invisible.material.transparency=BaseMaterial3D.TRANSPARENCY_DISABLED
-	#if camera: 
-		#camera.queue_free()
+	if camera: 
+		camera.queue_free()
